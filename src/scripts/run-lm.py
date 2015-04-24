d298c41ff8480d461d95bfb1ae69f277cfeb9484
VOLKAN CIRIK <vcirik@balina.ku.edu.tr>
2015-04-25 02:03:33 +0300

    lm file added. lm script added

---
+++ src/scripts/run-lm.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+'''
+  generate unigram features for another baseline
+  input : [1] a document file [2] lm file [3] file prefix 
+  output : tsv real-valued numbers
+'''
+import sys, os
+
+out_file = sys.argv[3]+sys.argv[1].split('/')[-1].split('.')[0]+'.f'
+text_file = sys.argv[1]
+lm_file = sys.argv[2]
+
+cmd = 'echo -e "perplexity -text '+ text_file + '" | evallm -binary '+lm_file + ' 2> /dev/null | grep Perplexity | cut -d "," -f1 | awk \'{print $3}\' >'+out_file
+os.system(cmd)
+
